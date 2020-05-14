7021fb6203a4462e8cb50f2e3c561c0019a4aa20
Anton <m@antonz.ru>
2020-05-14 20:12:12 +0300

    switched to suggestions 20.3

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
 // require suggest.js
-(function() {
+(function () {
     "use strict";
 
     /**
@@ -7,7 +7,7 @@
      * @param str
      * @returns {string}
      */
-    function b64_to_utf8( str ) {
+    function b64_to_utf8(str) {
         return decodeURIComponent(window.atob(str));
     }
 
@@ -16,31 +16,46 @@
      * @param $message jQuery-элемент сообщения
      */
     function showSubmitMessage($message) {
-        var chance = Math.floor(Math.random()*5) + 1;
+        var chance = Math.floor(Math.random() * 5) + 1;
         // пасхалка :-)
         if (chance === 5) {
-            var header = b64_to_utf8("JUQwJTk4JUQwJUI3JUQwJUIyJUQwJUI4JUQwJUJEJUQwJUI4JUQxJTgyJUQwJUI1JTJDJTIwJUQxJTgzJTIwJUQwJUJEJUQwJUIwJUQxJTgxJTIwJUQwJUJFJUQwJUIxJUQwJUI1JUQwJUI0");
-            var message = b64_to_utf8("JUQwJTlGJUQxJTgwJUQxJThGJUQwJUJDJUQwJUJFJTIwJUQxJTgxJUQwJUI1JUQwJUI5JUQxJTg3JUQwJUIwJUQxJTgxJTIwJUQwJUJDJUQxJThCJTIwJUQwJUJEJUQwJUI1JTIwJUQwJUJDJUQwJUJFJUQwJUI2JUQwJUI1JUQwJUJDJTIwJUQwJUJGJUQxJTgwJUQwJUI4JUQwJUJEJUQxJThGJUQxJTgyJUQxJThDJTIwJUQwJUIyJUQwJUIwJUQxJTg4JUQwJUI1JTIwJUQwJUJFJUQwJUIxJUQxJTgwJUQwJUIwJUQxJTg5JUQwJUI1JUQwJUJEJUQwJUI4JUQwJUI1LiUzQ2JyJTNFJUQwJTlEJUQwJUJFJTIwJUQwJUIyJUQxJThCJTIwJUQwJUIyJUQxJTgxJUQwJUI1JUQwJUIzJUQwJUI0JUQwJUIwJTIwJUQwJUJDJUQwJUJFJUQwJUI2JUQwJUI1JUQxJTgyJUQwJUI1JTIwJUQwJUJFJUQxJTgyJUQwJUJGJUQxJTgwJUQwJUIwJUQwJUIyJUQwJUI4JUQxJTgyJUQxJThDJTIwJUQwJUI1JUQwJUIzJUQwJUJFJTIwJUQwJUJEJUQwJUJFJUQxJTgyJUQwJUIwJUQxJTgwJUQwJUI4JUQwJUIwJUQwJUJCJUQxJThDJUQwJUJEJUQwJUJFJTIwJUQwJUI3JUQwJUIwJUQwJUIyJUQwJUI1JUQxJTgwJUQwJUI1JUQwJUJEJUQwJUJEJUQxJTgzJUQxJThFJTIwJUQwJUJBJUQwJUJFJUQwJUJGJUQwJUI4JUQxJThFJTIwJUQwJTlGJUQwJUJFJUQxJTg3JUQxJTgyJUQwJUJFJUQwJUI5JTIwJUQwJUEwJUQwJUJFJUQxJTgxJUQxJTgxJUQwJUI4JUQwJUI4JTIwJUQwJUJGJUQwJUJFJTIwJUQwJUIwJUQwJUI0JUQxJTgwJUQwJUI1JUQxJTgxJUQxJTgzJTNBJTIwMTI1MDMyJTJDJTIwJUQwJTlDJUQwJUJFJUQxJTgxJUQwJUJBJUQwJUIyJUQwJUIwJTJDJTIwJUQxJTgzJUQwJUJCLiUyMCVEMCVBMiVEMCVCMiVEMCVCNSVEMSU4MCVEMSU4MSVEMCVCQSVEMCVCMCVEMSU4RiUyQyUyMDEzLiUzQyUyRnAlM0U=");
+            var header = b64_to_utf8(
+                "JUQwJTk4JUQwJUI3JUQwJUIyJUQwJUI4JUQwJUJEJUQwJUI4JUQxJTgyJUQwJUI1JTJDJTIwJUQxJTgzJTIwJUQwJUJEJUQwJUIwJUQxJTgxJTIwJUQwJUJFJUQwJUIxJUQwJUI1JUQwJUI0"
+            );
+            var message = b64_to_utf8(
+                "JUQwJTlGJUQxJTgwJUQxJThGJUQwJUJDJUQwJUJFJTIwJUQxJTgxJUQwJUI1JUQwJUI5JUQxJTg3JUQwJUIwJUQxJTgxJTIwJUQwJUJDJUQxJThCJTIwJUQwJUJEJUQwJUI1JTIwJUQwJUJDJUQwJUJFJUQwJUI2JUQwJUI1JUQwJUJDJTIwJUQwJUJGJUQxJTgwJUQwJUI4JUQwJUJEJUQxJThGJUQxJTgyJUQxJThDJTIwJUQwJUIyJUQwJUIwJUQxJTg4JUQwJUI1JTIwJUQwJUJFJUQwJUIxJUQxJTgwJUQwJUIwJUQxJTg5JUQwJUI1JUQwJUJEJUQwJUI4JUQwJUI1LiUzQ2JyJTNFJUQwJTlEJUQwJUJFJTIwJUQwJUIyJUQxJThCJTIwJUQwJUIyJUQxJTgxJUQwJUI1JUQwJUIzJUQwJUI0JUQwJUIwJTIwJUQwJUJDJUQwJUJFJUQwJUI2JUQwJUI1JUQxJTgyJUQwJUI1JTIwJUQwJUJFJUQxJTgyJUQwJUJGJUQxJTgwJUQwJUIwJUQwJUIyJUQwJUI4JUQxJTgyJUQxJThDJTIwJUQwJUI1JUQwJUIzJUQwJUJFJTIwJUQwJUJEJUQwJUJFJUQxJTgyJUQwJUIwJUQxJTgwJUQwJUI4JUQwJUIwJUQwJUJCJUQxJThDJUQwJUJEJUQwJUJFJTIwJUQwJUI3JUQwJUIwJUQwJUIyJUQwJUI1JUQxJTgwJUQwJUI1JUQwJUJEJUQwJUJEJUQxJTgzJUQxJThFJTIwJUQwJUJBJUQwJUJFJUQwJUJGJUQwJUI4JUQxJThFJTIwJUQwJTlGJUQwJUJFJUQxJTg3JUQxJTgyJUQwJUJFJUQwJUI5JTIwJUQwJUEwJUQwJUJFJUQxJTgxJUQxJTgxJUQwJUI4JUQwJUI4JTIwJUQwJUJGJUQwJUJFJTIwJUQwJUIwJUQwJUI0JUQxJTgwJUQwJUI1JUQxJTgxJUQxJTgzJTNBJTIwMTI1MDMyJTJDJTIwJUQwJTlDJUQwJUJFJUQxJTgxJUQwJUJBJUQwJUIyJUQwJUIwJTJDJTIwJUQxJTgzJUQwJUJCLiUyMCVEMCVBMiVEMCVCMiVEMCVCNSVEMSU4MCVEMSU4MSVEMCVCQSVEMCVCMCVEMSU4RiUyQyUyMDEzLiUzQyUyRnAlM0U="
+            );
             $message.find("h4").text(header);
             $message.find("p[data-message]").html(message);
         } else {
             $message.find("h4").text("Это не настоящее правительство :-(");
-            $message.find("p[data-message]").html("К сожалению, мы не можем принять ваше обращение.<br>Но вы всегда можете отправить его через <a href=\"https://www.mos.ru/authority/treatments/reception/individuals/\">электронную приемную</a> правительства Москвы.");
+            $message
+                .find("p[data-message]")
+                .html(
+                    'К сожалению, мы не можем принять ваше обращение.<br>Но вы всегда можете отправить его через <a href="https://www.mos.ru/feedback/reception/">электронную приемную</a> правительства Москвы.'
+                );
         }
         $message.show();
     }
 
-    $(function() {
-        AddressSuggestions.init(
-            $("#address"),
-            [$("#address-region"), $("#address-city"), $("#address-street"), $("#address-house"), $("#address-flat")]
-        );
-        FullnameSuggestions.init(
-            $("#fullname"),
-            [$("#fullname-surname"), $("#fullname-name"), $("#fullname-patronymic")]
-        );
+    $(function () {
+        AddressSuggestions.init($("#address"), [
+            $("#address-region"),
+            $("#address-city"),
+            $("#address-street"),
+            $("#address-house"),
+            $("#address-flat"),
+        ]);
+        EmailSuggestions.init($("#email"));
+        FullnameSuggestions.init($("#fullname"), [
+            $("#fullname-surname"),
+            $("#fullname-name"),
+            $("#fullname-patronymic"),
+        ]);
         GranularFullnameSuggestions.init(
-            $("#fullname-surname"), $("#fullname-name"), $("#fullname-patronymic")
+            $("#fullname-surname"),
+            $("#fullname-name"),
+            $("#fullname-patronymic")
         );
 
         var $form = $("#feedback-form");
@@ -48,33 +63,25 @@
         var $progress = $("#feedback-progress");
         var $message = $("#feedback-message");
 
-        var phoneField = new ValidatedField("#phone", "PHONE", function(validatedObj) {
-            return validatedObj.phone;
-        });
-
-        var emailField = new ValidatedField("#email", "EMAIL", function(validatedObj) {
-            return validatedObj.email;
-        });
-
-        $message.find("[data-action='ok']").click(function() {
+        $message.find("[data-action='ok']").click(function () {
             $message.hide();
         });
 
-        $form.submit(function(e) {
+        $form.submit(function (e) {
             e.preventDefault();
             $btnSubmit.hide();
             $message.hide();
             $progress.parent().show();
             var progressVal = 5;
             var timer;
-            var setProgress = function(val) {
+            var setProgress = function (val) {
                 progressVal = val;
-                $progress.css('width', val+'%');
+                $progress.css("width", val + "%");
             };
-            var forward = function() {
-                setProgress(progressVal+5);
-                if(progressVal < 100) {
-                    timer = setTimeout(forward, 100)
+            var forward = function () {
+                setProgress(progressVal + 5);
+                if (progressVal < 100) {
+                    timer = setTimeout(forward, 100);
                 } else {
                     $progress.parent().hide();
                     setProgress(0);
@@ -83,8 +90,6 @@
                 }
             };
             timer = setTimeout(forward, 100);
-
         });
     });
-
 })();
--- js/suggest.js
+++ js/suggest.js
@@ -1,6 +1,7 @@
-// require dadata.js
-(function() {
+(function () {
     "use strict";
+
+    var TOKEN = "7fd18aaabd7d53ffa4846e4521c1f736c13490eb";
 
     /**
      * Объединяет элементы массива через разделитель. При этом игнорирует пустые элементы.
@@ -10,7 +11,11 @@
      */
     function join(arr /*, separator */) {
         var separator = arguments.length > 1 ? arguments[1] : ", ";
-        return arr.filter(function(n){return n}).join(separator);
+        return arr
+            .filter(function (n) {
+                return n;
+            })
+            .join(separator);
     }
 
     /**
@@ -25,71 +30,46 @@
          * @param separator Разделитель, через который нужно объединять гранулярные части
          * @constructor
          */
-        init: function($el, parts, separator) {
-            parts.forEach(function($part) {
-                $part.change(function() {
-                    var partialValues = parts.map(
-                        function($el) { return $el.val() }
-                    );
-                    $el.val(
-                        join(partialValues, separator)
-                    );
+        init: function ($el, parts, separator) {
+            parts.forEach(function ($part) {
+                $part.change(function () {
+                    var partialValues = parts.map(function ($el) {
+                        return $el.val();
+                    });
+                    $el.val(join(partialValues, separator));
                 });
             });
-        }
+        },
     };
-
-    $.Suggestions.dadataConfig.url = DadataApi.DADATA_API_URL + "/clean";
 
     /**
      * Подскази по адресу
      */
     var AddressSuggestions = {
-
         /**
          * Инициализирует подсказки по адресу на указанном элементе
          * @param $el   jQuery-элемент ввода адреса одной строкой
          * @param parts Массив jQuery-элементов для гранулярных частей адреса
          * @constructor
          */
-        init: function($el, parts) {
+        init: function ($el, parts) {
             var self = this;
             Suggestions.init.call(self, $el, parts, ", ");
             $el.suggestions({
-                serviceUrl: DadataApi.DADATA_API_URL + "/suggest/address",
-                token: DadataApi.TOKEN,
+                token: TOKEN,
                 type: "ADDRESS",
-                useDadata: true,
-                maxHeight: 340,
-                onSearchStart: self.forceMoscow,
-                transformResult: self.trimResults,
+                constraints: {
+                    locations: {
+                        kladr_id: "7700000000000",
+                    },
+                },
                 formatResult: self.formatResult,
-                onSelect: function(suggestion) {
+                onSelect: function (suggestion) {
                     if (suggestion.data) {
                         self.showSelected(suggestion);
                     }
-                }
+                },
             });
-        },
-
-        /**
-         * Ограничивает поиск Москвой
-         * @param params Параметры ajax-запроса
-         */
-        forceMoscow: function (params) {
-            var pattern = /Москва/i;
-            if (!pattern.test(params.query)) {
-                params.query = "Москва " + params.query;
-            }
-        },
-
-        /**
-         * Фильтрует список подсказок
-         * @param response Ответ от сервера подсказок
-         */
-        trimResults: function (response) {
-            response.suggestions.splice(7,3);
-            return response;
         },
 
         /**
@@ -99,28 +79,39 @@
          * @param currentValue Введенный пользователем текст
          * @returns {string} HTML для элемента списка подсказок
          */
-        formatResult: function (suggestion, currentValue) {
+        formatResult: function (value, currentValue, suggestion, options) {
             suggestion.value = suggestion.value.replace("Россия, ", "");
             var address = suggestion.data;
+            var city =
+                (address.city !== address.region && join([address.city_type, address.city], " ")) ||
+                "";
             // первая строка - регион, район, город
-            var part1 = join([
-                address.region,
-                join([address.area_type, address.area], " "),
-                join([address.city_type, address.city], " ")
-            ]);
+            var part1 = join([address.region, join([address.area_type, address.area], " "), city]);
             // вторая строка - населенный пункт, улица, дом, квартира
             var part2 = join([
                 join([address.settlement_type, address.settlement], " "),
                 join([address.street_type, address.street], " "),
                 join([address.house_type, address.house], " "),
                 join([address.block_type, address.block], " "),
-                join([address.flat_type, address.flat], " ")
+                join([address.flat_type, address.flat], " "),
             ]);
             // подсветка введенного пользователем текста
-            var pattern = '(^|\\s+)(' + $.Suggestions.utils.escapeRegExChars(currentValue) + ')';
-            part2 = part2.replace(new RegExp(pattern, 'gi'), '$1<strong>$2<\/strong>')
-            var suggestedValue = part2 ?
-                "<span class=\"suggestions-suggestion-region\">" + part1 + "</span>" + "<br>&nbsp;&nbsp;" + part2
+            part1 = $.Suggestions.prototype.highlightMatches.call(
+                this,
+                part1,
+                currentValue,
+                suggestion,
+                options
+            );
+            part2 = $.Suggestions.prototype.highlightMatches.call(
+                this,
+                part2,
+                currentValue,
+                suggestion,
+                options
+            );
+            var suggestedValue = part2
+                ? part2 + '<br><span class="suggestions-suggestion-region">' + part1 + "</span>"
                 : part1;
             return suggestedValue;
         },
@@ -132,63 +123,60 @@
         showSelected: function (suggestion) {
             var address = suggestion.data;
             $("#address-postal_code").val(address.postal_code);
-            $("#address-region").val(
-                join([address.region_type, address.region], " ")
+            $("#address-region").val(join([address.region_type, address.region], " "));
+            $("#address-city").val(
+                join([
+                    join([address.area_type, address.area], " "),
+                    join([address.city_type, address.city], " "),
+                    join([address.settlement_type, address.settlement], " "),
+                ])
             );
-            $("#address-city").val(join([
-                join([address.area_type, address.area], " "),
-                join([address.city_type, address.city], " "),
-                join([address.settlement_type, address.settlement], " ")
-            ]));
-            $("#address-street").val(
-                join([address.street_type, address.street], " ")
+            $("#address-street").val(join([address.street_type, address.street], " "));
+            $("#address-house").val(
+                join([
+                    join([address.house_type, address.house], " "),
+                    join([address.block_type, address.block], " "),
+                ])
             );
-            $("#address-house").val(join([
-                join([address.house_type, address.house], " "),
-                join([address.block_type, address.block], " ")
-            ]));
-            $("#address-flat").val(
-                join([address.flat_type, address.flat], " ")
-            );
-        }  // любите работать с кодом? У нас есть отличные вакансии http://hh.ru/employer/15589!
+            $("#address-flat").val(join([address.flat_type, address.flat], " "));
+        }, // любите работать с кодом? У нас есть отличные вакансии http://hh.ru/employer/15589!
+    };
+
+    /**
+     * Подсказки по email
+     */
+    var EmailSuggestions = {
+        init: function ($el) {
+            $el.suggestions({
+                token: TOKEN,
+                type: "EMAIL",
+                suggest_local: false,
+            });
+        },
     };
 
     /**
      * Подсказки по ФИО
      */
     var FullnameSuggestions = {
-
         /**
          * Инициализирует подсказки по ФИО на указанном элементе
          * @param $el   jQuery-элемент ввода ФИО одной строкой
          * @param parts Массив jQuery-элементов для гранулярных частей ФИО
          * @constructor
          */
-        init: function($el, parts) {
+        init: function ($el, parts) {
             var self = this;
             Suggestions.init.call(self, $el, parts, " ");
             $el.suggestions({
-                serviceUrl: DadataApi.DADATA_API_URL + "/suggest/fio",
-                token: DadataApi.TOKEN,
+                token: TOKEN,
                 type: "NAME",
-                useDadata: true,
-                maxHeight: 340,
-                transformResult: self.trimResults,
-                onSelect: function(suggestion) {
+                onSelect: function (suggestion) {
                     if (suggestion.data) {
                         self.showSelected(suggestion);
                     }
-                }
+                },
             });
-        },
-
-        /**
-         * Фильтрует список подсказок
-         * @param response Ответ от сервера подсказок
-         */
-        trimResults: function (response) {
-            response.suggestions.splice(7,3);
-            return response;
         },
 
         /**
@@ -200,14 +188,13 @@
             $("#fullname-surname").val(fullname.surname);
             $("#fullname-name").val(fullname.name);
             $("#fullname-patronymic").val(fullname.patronymic);
-        }
+        },
     };
 
     /**
      * Гранулярные подсказки по ФИО
      */
     var GranularFullnameSuggestions = {
-
         /**
          * Инициализирует подсказки по ФИО на указанном элементе
          * @param $surname jQuery-элемент для текстового поля с фамилией
@@ -215,7 +202,7 @@
          * @param $patronymic jQuery-элемент для текстового поля с отчеством
          * @constructor
          */
-        init: function($surname, $name, $patronymic) {
+        init: function ($surname, $name, $patronymic) {
             var self = this;
             self.$surname = $surname;
             self.$name = $name;
@@ -223,26 +210,26 @@
             var fioParts = ["SURNAME", "NAME", "PATRONYMIC"];
             // инициализируем подсказки на всех трех текстовых полях
             // (фамилия, имя, отчество)
-            $.each([$surname, $name, $patronymic], function(index, $el) {
+            $.each([$surname, $name, $patronymic], function (index, $el) {
                 $el.suggestions({
-                    serviceUrl: DadataApi.DADATA_API_URL + "/suggest/fio",
-                    token: DadataApi.TOKEN,
+                    token: TOKEN,
+                    type: "NAME",
                     hint: "",
                     noCache: true,
                     params: {
                         // каждому полю --- соответствующая подсказка
-                        parts: [fioParts[index]]
+                        parts: [fioParts[index]],
                     },
-                    onSearchStart: function(params) {
+                    onSearchStart: function (params) {
                         // если пол известен на основании других полей,
                         // используем его
                         var $el = $(this);
                         params.gender = self.isGenderKnown($el) ? self.gender : "UNKNOWN";
                     },
-                    onSelect: function(suggestion) {
+                    onSelect: function (suggestion) {
                         // определяем пол по выбранной подсказке
                         self.gender = suggestion.data.gender;
-                    }
+                    },
                 });
             });
         },
@@ -252,23 +239,25 @@
          * @param $el элемент, в котором находится фокус курсора
          * @returns {boolean}
          */
-        isGenderKnown: function($el) {
+        isGenderKnown: function ($el) {
             var self = this;
             var surname = self.$surname.val(),
                 name = self.$name.val(),
                 patronymic = self.$patronymic.val();
-            if (($el.attr('id') == self.$surname.attr('id') && !name && !patronymic) ||
-                ($el.attr('id') == self.$name.attr('id') && !surname && !patronymic) ||
-                ($el.attr('id') == self.$patronymic.attr('id') && !surname && !name)) {
+            if (
+                ($el.attr("id") == self.$surname.attr("id") && !name && !patronymic) ||
+                ($el.attr("id") == self.$name.attr("id") && !surname && !patronymic) ||
+                ($el.attr("id") == self.$patronymic.attr("id") && !surname && !name)
+            ) {
                 return false;
             } else {
                 return true;
             }
-        }
+        },
     };
 
     window.AddressSuggestions = AddressSuggestions;
+    window.EmailSuggestions = EmailSuggestions;
     window.FullnameSuggestions = FullnameSuggestions;
     window.GranularFullnameSuggestions = GranularFullnameSuggestions;
-
 })();
